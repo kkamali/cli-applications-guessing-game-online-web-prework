34d05215366710d18b5ca98b2094a0d42d835e52
Kathleen Kamali <kathleenkamali@gmail.com>
2019-09-17 18:22:13 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,5 +3,7 @@ def run_guessing_game
   random_number = rand(6) + 1 
   puts "Guess a number between 1 and 6!"
   guess = gets.chomp
-  puts guess
+  if guess == "exit"
+    puts "Goodbye!"
+  end
 end
